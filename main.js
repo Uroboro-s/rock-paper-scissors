cdd3c707839cb74a83dfdc0ad87b0c27de652506
JANMEYJAI MISHRA <117590730+Uroboro-s@users.noreply.github.com>
2023-01-26 15:18:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,55 +1,136 @@
 function getComputerChoice()
 {
     let random=Math.random();
-    console.log(random);
+    
     if(random<1/3)
     {
+        document.getElementById('compSelect').src='images/rock.png';
         return "ROCK";
     }
     else if(random>=1/3&&random<2/3)
     {
+        document.getElementById('compSelect').src='images/paper.png';
         return "PAPER";
     }
     else 
     {
+        document.getElementById('compSelect').src='images/scissors.png';
         return "SCISSORS";
     }
-
+    
 }
-console.log(getComputerChoice());
+
 function declareWinner(playerSelection, computerSelection)
 {
-    let x=playerSelection.toUpperCase();
+    let x=playerSelection;
     
     if(x=="ROCK"&&computerSelection=="SCISSORS")
+    {
+        calculatePoints(1);
      return "You Win! Rock beats Scissors";
-    
+    }
     else if(x=="ROCK"&&computerSelection=="PAPER")
-     return "You lose! Paper beats Rock";
+     {
+        calculatePoints(0);
+        return "You lose! Paper beats Rock";
+     }
 
     else if(x=="PAPER"&&computerSelection=="ROCK")
-     return "You Win! Paper beats Rock";
-
+     {
+        calculatePoints(1);
+        return "You Win! Paper beats Rock";
+     }
     else if(x=="PAPER"&&computerSelection=="SCISSORS")
-     return "You Lose! Scissors beats Paper";
-
+     {
+        calculatePoints(0);
+        return "You Lose! Scissors beats Paper";
+    }
     else if(x=="SCISSORS"&&computerSelection=="ROCK")
-     return "You Lose! Rock beats Scissors";
-    
+     {
+        calculatePoints(0);
+        return "You Lose! Rock beats Scissors";
+      }
     else if (x=="SCISSORS"&&computerSelection=="PAPER")
-     return "You Win! Scissors beats Paper";
-
+     {
+        calculatePoints(1);
+        return "You Win! Scissors beats Paper";
+    }
     else
-    return "";
+    
+     return "Ughhh! It's a tie! So boring...";
+     
+}
+
+
+function calculatePoints(key)
+{
+    const playerPoints=document.querySelector('#playerPoints');
+    const compPoints=document.querySelector('#computerPoints');
+
+    if(key==1)
+    {
+        playerPoints.textContent++;
+    }
+    else
+    {
+        compPoints.textContent++;
+
+    }
+}
+
+function resetPoints()
+{
+const playerPoints=document.querySelector('#playerPoints');
+const compPoints=document.querySelector('#computerPoints');
+const result=document.querySelector('#result');
+result.textContent="BEGIN";
+playerPoints.textContent="0";
+compPoints.textContent="0";
+document.getElementById('playerSelect').src="";
+document.getElementById('compSelect').src="";
+}
+
+
+function onRock()
+{
+    document.getElementById('playerSelect').src='images/rock.png';
+    var computerChoice=getComputerChoice();
+    let displayContent=declareWinner("ROCK", computerChoice);
+    console.log(displayContent);
+    var display=document.querySelector('#result');
+    display.textContent=displayContent;
+    
+}
+function onPaper()
+{
+    document.getElementById('playerSelect').src='images/paper.png';
+    var computerChoice=getComputerChoice();
+    let displayContent=declareWinner("PAPER", computerChoice);
+    var display=document.querySelector('#result');
+    display.textContent=displayContent;
+
+}
+function onScissors()
+{
+    document.getElementById('playerSelect').src='images/scissors.png';
+    var computerChoice=getComputerChoice();
+    let displayContent=declareWinner("SCISSORS", computerChoice);
+    var display=document.querySelector('#result');
+    display.textContent=displayContent;
+
 }
 function game()
 {
-    for(let i=1;i<=5;i++)
-    {
-        let playerSelection=prompt("Throw either Rock or Paper or Scissors!");
-        let computerChoice=getComputerChoice();
+    
+    const rock=document.querySelector('#rock');
+    const paper=document.querySelector('#paper');
+    const scissors=document.querySelector('#scissors');
+    const reset=document.querySelector('#restart');
+     rock.addEventListener('click', onRock);
+     paper.addEventListener('click', onPaper);
+     scissors.addEventListener('click', onScissors);
+     restart.addEventListener('click', resetPoints);
         
-       console.log(declareWinner(playerSelection, computerChoice));
-     
-    }
+       
 }
+game();
